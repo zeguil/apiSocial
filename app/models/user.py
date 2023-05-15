1da70f8a816c52cbc81ec64add75d7181d9e23cc
Jose Guilherme <zeguilhermelins@hotmail.com>
2023-05-15 07:49:33 -0400

    add model profile

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+    profile = relationship("Profile", uselist=False, back_populates="user")
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True)
-    full_name = Column(String(100))
-    bio = Column(String(280))
-    location = Column(String(100))
-
-    def __repr__(self):
-        return f"<User(id={self.id}, username='{self.username}', full_name='{self.full_name}')>"
+class Profile(Base):
+    __tablename__ = 'profiles'
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String)
+    bio = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="profile")
