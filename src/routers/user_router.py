89fb7805a16597541b9a94ad5bf1f8f8829e4a74
Jose Guilherme <zeguilhermelins@hotmail.com>
2023-05-29 17:01:31 -0400

    add exceptions

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -7,6 +7,7 @@ from sqlalchemy import or_
 from models.user import User
 from utils.user_utils import (validate_user_data)
 import bcrypt
+from logs.logger import logger
 
 class UserRequest(BaseModel):
     username: str
@@ -37,65 +38,94 @@ def list_users(db: Session = Depends(get_db)) -> List[UserResponse]:
 @userRouter.get("/" ,response_model=UserResponse, status_code=200)
 def list_user_by_id(id_user: int, db: Session = Depends(get_db)) -> UserResponse:
 
-    user = db.query(User).get(id_user)
+    try:
+        user = db.query(User).get(id_user)
 
-    # Verifica se o usuário existe no banco
-    if not user:
-        raise HTTPException(status_code=404, detail="item not found")
+        # Verifica se o usuário existe no banco
+        if not user:
+            raise HTTPException(status_code=404, detail="item not found")
 
-    return user
-
+        return user
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error"
+                            
 #! Create User
 @userRouter.post("/", response_model= UserResponse, status_code=201)
 def create_user(user: UserRequest, db: Session = Depends(get_db)) -> UserResponse:
 
     validate_user_data(user)
     
-    existing_user = db.query(User).filter(or_(User.username == user.username, User.email == user.email)).first()
-    if existing_user:
-        if existing_user.username == user.username:
-            raise HTTPException(status_code=409, detail="Username already in use")
-        else:
-            raise HTTPException(status_code=409, detail="Email already in use")
-    
-    # Criptografa a senha antes de salvar no banco
-    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    try:
+        # Verifica se usuario ou email ja existem no banco
+        existing_user = db.query(User).filter(or_(User.username == user.username,
+                                                User.email == user.email)
+                                                ).first()
+        if existing_user:
+            if existing_user.username == user.username:
+                raise HTTPException(status_code=409, detail="Username already in use")
+            else:
+                raise HTTPException(status_code=409, detail="Email already in use")
+        
+        # Criptografa a senha antes de salvar no banco
+        hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
 
-    new_user = User(
-        username=user.username,
-        password=hashed_password.decode('utf-8'),
-        email = user.email
-    )
-    
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+        new_user = User(
+            username=user.username,
+            password=hashed_password.decode('utf-8'),
+            email = user.email
+        )
+        
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
 
-    return new_user
+        return new_user
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
 
 #! Update User
 @userRouter.put("/{id_user}", response_model=UserResponse,  status_code=200)
 def update_user(id_user: int, user_request: UserRequest, db: Session = Depends(get_db)) -> UserResponse:
     
-    user: User =  db.query(User).get(id_user)
-    # Campos a serem atualizados
-    user.username = user_request.username
-    user.password = user_request.password
-    user.is_admin = user_request.is_admin
+    
+    try:
+        user: User =  db.query(User).get(id_user)
 
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return user
+        if not user:
+            raise HTTPException(status_code=404, detail="Item not found")
+        
+        if not id_user:
+            raise HTTPException(status_code=404, detail="item not found")
+        
+        # Campos a serem atualizados
+        user.username = user_request.username
+        user.password = user_request.password
+        user.is_admin = user_request.is_admin
+
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=400, detail="not found")
 
 #! Delete User
 @userRouter.delete("/{id_user}", status_code=204)
 def delete_user(id_user: int, db: Session = Depends(get_db)) -> None:
+    try:
+        user = db.query(User).get(id_user)
 
-    user = db.query(User).get(id_user)
-    if user:
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        
         db.delete(user)
         db.commit()
 
-        return {"msg": "deleted user"}
+        return {"msg": "User deleted"}
+    
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=400, detail="Bad request")
     
